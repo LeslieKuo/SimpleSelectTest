93c68c3ea762bb29b521b9f849bfef141398f1d8
LeslieKuo <676365950@qq.com>
2017-10-10 20:12:43 +0800

    SimpleSelect
    A simple select( )test on Ubuntu

---
+++ ex4_selectTest/build-simpleSelect-Desktop_Qt_5_9_1_GCC_64bit-Default/CMakeFiles/simpleSelect.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/maru/Desktop/wangkou/QtPro/ex4_selectTest/simpleSelect/main.c" "/home/maru/Desktop/wangkou/QtPro/ex4_selectTest/build-simpleSelect-Desktop_Qt_5_9_1_GCC_64bit-Default/CMakeFiles/simpleSelect.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ex4_selectTest/build-simpleSelect-Desktop_Qt_5_9_1_GCC_64bit-Default/CMakeFiles/simpleSelect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpleSelect.dir/main.c.o"
+  "simpleSelect.pdb"
+  "simpleSelect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simpleSelect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
